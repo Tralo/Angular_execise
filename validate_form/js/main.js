9108af81fa9eb783bce8065ecf074d8121dc8af1
tralo <13580548169@163.com>
2016-08-06 13:08:29 +0800

    update

--- validate_form/js/main.js
+++ validate_form/js/main.js
@@ -8,5 +8,45 @@ app.controller('SignController',function($scope){
 
 	$scope.submitForm = function(){
 		console.log($scope.userdata);
+		if($scope.signUpForm.$invalid){
+			alert('请检查您的信息');
+		}　else {
+			alert('提交成功!');
+		}
+
 	};
-});
+})
+.directive('compare',function(){
+var o = {};
+o.strict = 'AE';
+o.scope = {
+	orgText:'=compare'
+}
+o.require = 'ngModel';
+o.link = function(sco,ele,att,con){
+con.$validators.compare = function(v){
+return v == sco.orgText;
+}
+sco.$watch('orgText',function(){
+con.$validate();
+})
+}
+return o;
+})
+/*.directive('compare',function(){
+var o = {};
+o.strict = 'AE';
+o.scope = {
+orgText: '=compare'
+}
+o.require = 'ngModel';
+o.link = function(sco, ele, att, con){
+con.$validators.compare = function(v){
+return v == sco.orgText;
+}
+sco.$watch('orgText', function(){
+con.$validate();
+})
+}
+return o;
+})*/
